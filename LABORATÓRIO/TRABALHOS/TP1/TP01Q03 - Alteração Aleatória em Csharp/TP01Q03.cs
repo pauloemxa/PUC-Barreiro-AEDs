48edd5dd63b94196858dcad48c93a47056de21c4
paulocasa <paulo@logpyx.com>
2022-09-11 21:16:47 -0300

    asd

--- LABORATÓRIO/TRABALHOS/TP1/TP01Q03 - Alteração Aleatória em Csharp/TP01Q03.cs
+++ LABORATÓRIO/TRABALHOS/TP1/TP01Q03 - Alteração Aleatória em Csharp/TP01Q03.cs
@@ -5,15 +5,10 @@ namespace TP01
 {
     class Q03
     {
-        static Random gerador = new Random(4);
-        static char aleatorio1 = (char)('a' + (Math.Abs(gerador.Next()) % 26));
-        static char aleatorio2 = (char)('a' + (Math.Abs(gerador.Next()) % 26));
 
-        public static String ciframentoAleatorio(String word)
+
+        public static String ciframentoAleatorio(String word, char aleatorio1, char aleatorio2)
         {
-
-
-
             String resp = "";
 
             for (int i = 0; i < word.Length; i++)
@@ -32,6 +27,10 @@ namespace TP01
 
         static void Main(string[] args)
         {
+            Random gerador = new Random(4);
+            char aleatorio1 = (char)('a' + (Math.Abs(gerador.Next()) % 26));
+            char aleatorio2 = (char)('a' + (Math.Abs(gerador.Next()) % 26));
+
             System.Console.WriteLine("valores: " + aleatorio1 + " e " + aleatorio2);
             String linha = "";
 
@@ -39,7 +38,7 @@ namespace TP01
             {
                 linha = Console.ReadLine();
                 if (linha != "FIM")
-                    System.Console.WriteLine(ciframentoAleatorio(linha));
+                    System.Console.WriteLine(ciframentoAleatorio(linha, aleatorio1, aleatorio2));
             }
         }
     }
